05f2ac14b2a79843b2773f8bd87b3b5f5272cf25
Patrícia Gonçalves <47126842+gonpatricia@users.noreply.github.com>
2023-05-22 16:38:49 +0100

    Update data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -108,7 +108,7 @@ ad_long <- ad %>%
   add_modal_linearweight(varmod = "Maturity", config$ma_method) %>%
   add_modal_negexpweight(varmod = "Maturity", config$ma_method)
 
-ad_long_ex <- ad[ad$expertise == "Advanced", ] %>%
+ad_long_adv <- ad[ad$expertise == "Advanced", ] %>% 
   add_modal_trad(varmod = "Maturity", config$ma_method) %>%
   add_modal_linearweight(varmod = "Maturity", config$ma_method) %>%
   add_modal_negexpweight(varmod = "Maturity", config$ma_method)
@@ -120,14 +120,14 @@ ad_long <- ad_long %>%
   add_modal_linearweight(varmod = "Sex", config$ma_method) %>%
   add_modal_negexpweight(varmod = "Sex", config$ma_method)
 
-ad_long_ex <- ad_long_ex %>%
+ad_long_adv <- ad_long_adv %>%
   add_modal_trad(varmod = "Sex", config$ma_method) %>%
   add_modal_linearweight(varmod = "Sex", config$ma_method) %>%
   add_modal_negexpweight(varmod = "Sex", config$ma_method)
 
 # Choose the final mode (traditional, readers linear weight or negative exponential linear weight) based in the existence of histological samples or not, and, in case there are no histological samples, depending if there is multimodality or not.
 ad_long <- select_mode(ad_long, config$ma_method, config$mode_definition)
-ad_long_ex <- select_mode(ad_long_ex, config$ma_method, config$mode_definition)
+ad_long_adv <- select_mode(ad_long_adv, config$ma_method, config$mode_definition)
 
 # prepare data in wbgr output format
 # IMAGE,1,2,3,4,5,6,7,8,9,10,11,12,13
@@ -164,6 +164,6 @@ head(webgr_sex)
 # write out input data tables for use later
 write.taf(ad4webgr, "data/data.csv", quote = TRUE)
 write.taf(ad_long, "data/ad_long.csv", quote = TRUE)
-write.taf(ad_long_ex, "data/ad_long_ex.csv", quote = TRUE)
+write.taf(ad_long_adv, "data/ad_long_adv.csv", quote = TRUE)
 write.taf(webgr_maturity, "data/WebGR_maturity_ages_all.csv", quote = TRUE)
 write.taf(webgr_sex, "data/WebGR_sex_ages_all.csv", quote = TRUE)
