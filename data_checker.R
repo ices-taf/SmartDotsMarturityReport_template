d7b4bc31ef6167e936df4808efa2177c6ab18b89
Patrícia Gonçalves <47126842+gonpatricia@users.noreply.github.com>
2023-05-24 10:59:24 +0100

    Update data_checker.R

--- data_checker.R
+++ data_checker.R
@@ -11,7 +11,8 @@ config <- read_json("bootstrap/data/config.json", simplifyVector = TRUE)
 
 # get data from bootstrap folder  -------------------------------
 #ad <- read.taf("bootstrap/smartdots_db/ad.csv")
-ad <- read.taf("bootstrap/data/ad.csv")
+ad <- read.taf("bootstrap/data/smartdots_db/ad.csv")
+#ad <- read.taf("bootstrap/data/ad.csv")
 
 # tag some feilds as missing?
 
