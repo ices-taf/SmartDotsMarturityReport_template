65d9521846ba56324c645c79432428be1d11de73
Patrícia Gonçalves <47126842+gonpatricia@users.noreply.github.com>
2023-05-24 12:27:51 +0100

    Update data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -96,6 +96,7 @@ for (i in 1:length(fishid))
 ad <- result
 ad$TypeAnnotation[ad$TypeAnnotation=="Reader"]<-"reader"
 ad$TypeAnnotation[ad$TypeAnnotation=="Delegate"]<-"eventOrganizer"
+ad$TypeAnnotation[ad$TypeAnnotation=="eventDelegate"]<-"eventOrganizer"
 ad$TypeAnnotation[ad$TypeAnnotation=="Organizer"]<-"eventOrganizer"
 ad$reader[ad$reader==""]<-"eventOrganizer" ## to add a name to the Reader column from the Event Organizer
 ad$reader[ad$TypeAnnotation=="eventOrganizer"]<-""
