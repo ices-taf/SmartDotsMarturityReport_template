d95e4d07ade08ce0b58c058e68e0b675c8a3a3c7
Patrícia Gonçalves <47126842+gonpatricia@users.noreply.github.com>
2023-05-24 11:00:03 +0100

    Update data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -30,7 +30,8 @@ config <- read_json("bootstrap/data/config.json", simplifyVector = TRUE)
 
 # get data from bootstrap folder  -------------------------------
 #ad <- read.taf("bootstrap/data/smartdots_db/ad.csv")
-ad <- read.taf("bootstrap/data/ad.csv")
+ad <- read.taf("bootstrap/data/smartdots_db/ad.csv")
+#ad <- read.taf("bootstrap/data/ad.csv")
 
 # prepare data -------------------------------
 
