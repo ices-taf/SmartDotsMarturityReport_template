9307e4e76843af24e16ea0c53582b291fe1a5d2f
Patrícia Gonçalves <47126842+gonpatricia@users.noreply.github.com>
2024-01-31 14:43:25 +0000

    Update utilities_model.R

--- utilities_model.R
+++ utilities_model.R
@@ -357,10 +357,12 @@ general_freq_table <- function(ad_long, varmod, by=NULL) {
 data_overview_table <- function(dat, varmod, report_token) {
 
   #if (any(dat$TypeAnnotation == "eventOrganizer")) {
-  if (any(dat$TypeAnnotation == "eventOrganizer" & dat$DoesSampleHaveHistologyImage == "Yes")) {
+ # if (any(dat$TypeAnnotation == "eventOrganizer" & dat$DoesSampleHaveHistologyImage == "Yes")) {
+  if (any(dat$DoesSampleHaveHistologyImage == "Yes")) { ##new -31jan2024
     hist <-
       #dat[dat$TypeAnnotation == "eventOrganizer", ] %>%
-      dat[dat$TypeAnnotation == "eventOrganizer" & dat$DoesSampleHaveHistologyImage == "Yes", ] %>%
+      #dat[dat$TypeAnnotation == "eventOrganizer" & dat$DoesSampleHaveHistologyImage == "Yes", ] %>%
+     dat[dat$DoesSampleHaveHistologyImage == "Yes", ] %>% ##new -31jan2024
     #  ddply(.(SampleID), summarise, Histology = "yes") %>%
       ddply(.(FishID), summarise, Histology = "yes") %>%
       #select(., c("SampleID", "Histology"))
