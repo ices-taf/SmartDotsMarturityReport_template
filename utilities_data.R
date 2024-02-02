038f418602a5ac33630680b4ce23b4f6342836d8
Patrícia Gonçalves <47126842+gonpatricia@users.noreply.github.com>
2024-02-02 10:59:35 +0000

    Update utilities_data.R

--- utilities_data.R
+++ utilities_data.R
@@ -10,18 +10,18 @@
 # If the modal age is 0 the CV is set to 0 as well.
 
 add_modal_trad <- function(ad, varmod, ma_method) {
-  
   # ages by fish
   out <-
     ad %>%
-    select(FishID, reader, all_of(varmod)) %>%
-    ddply(.(FishID, get(all_of(varmod))), summarise, count=length(reader)) %>%
+    select(FishID, SampleID, reader, all_of(varmod)) %>%
+    ddply(.(FishID, SampleID, get(all_of(varmod))), summarise, count=length(reader)) %>%
     spread(key = "get(all_of(varmod))", value = count)
-  
+
   out[is.na(out)]=0
-  
-  datsel <- out %>% select(-c(FishID))
-  
+
+   datsel <- out %>% select(-c(FishID, SampleID))
+
+
   # Determine modal maturity stage depending on ma_method
   out$modal_trad <-
     if (ma_method == "Mean") {
@@ -37,41 +37,41 @@ add_modal_trad <- function(ad, varmod, ma_method) {
             })
     }
 
-  
+
   countcases=vector(length=dim(out)[1])
   for(e in 1:dim(out)[1])
   {
     sel=out[e,]
     df=sel[,-c(1,2,dim(out)[2])]
     max=max(df)
-    countcases[e]=length(df[which(df==max & df!=0)])
+    countcases[e]=length(df[which(df==max)])
   }
-  
+
   out$NModes_trad=countcases
-  
+
   # calculate CU (coefficient of unalikeability)
   out$cu <- apply(datsel, 1, cu_I)
-  
+
   colnames(out)=c(names(out)[-c((length(names(out))-2):length(names(out)))], paste0("modal_trad_", varmod), paste0("NModes_trad_", varmod), paste0("cu_", varmod))
   # merge CV and modal age to data
-  right_join(ad, out, by = "FishID")
+  right_join(ad, out, by = c("FishID", "SampleID"))
 }
 
 
 
 add_modal_linearweight <- function(ad, varmod, ma_method) {
-  
+
   # ages by fish
   out <-
     ad %>%
-    select(FishID, weight_I, all_of(varmod)) %>%
-    ddply(.(FishID, get(all_of(varmod))), summarise, readerweight=sum(weight_I)) %>%
+    select(FishID, SampleID, weight_I, all_of(varmod)) %>%
+    ddply(.(FishID, SampleID, get(all_of(varmod))), summarise, readerweight=sum(weight_I)) %>%
     spread(key = "get(all_of(varmod))", value = readerweight)
-  
+
   out[is.na(out)]=0
-  
-  datsel <- out %>% select(-c(FishID))
-  
+
+  datsel <- out %>% select(-c(FishID, SampleID))
+
   # Determine modal maturity stage depending on ma_method
   out$modal_linearweight <-
     if (ma_method == "Mean") {
@@ -86,39 +86,40 @@ add_modal_linearweight <- function(ad, varmod, ma_method) {
               }
             })
     }
-  
-  
+
+
   countcases=vector(length=dim(out)[1])
   for(e in 1:dim(out)[1])
   {
     sel=out[e,]
     df=sel[,-c(1, 2, dim(out)[2])]
     max=max(df)
-    countcases[e]=length(df[which(df==max & df!=0)])
+    countcases[e]=length(df[which(df==max)])
   }
-  
+
   out$NModes_linear=countcases
-  
+
   colnames(out)=c(names(out)[-c((length(names(out))-1):length(names(out)))], paste0("modal_linearweight_", varmod), paste0("NModes_linearweight_", varmod))
   # merge CV and modal age to data
-  right_join(ad, out, by = "FishID")
+  right_join(ad, out, by = c("FishID", "SampleID"))
 }
 
 
 
 add_modal_negexpweight <- function(ad, varmod, ma_method) {
-  
+
   # ages by fish
   out <-
     ad %>%
-    select(FishID,  weight_II, all_of(varmod)) %>%
-    ddply(.(FishID,  get(all_of(varmod))), summarise, readerweight=sum(weight_II)) %>%
+    select(FishID, SampleID, weight_II, all_of(varmod)) %>%
+    ddply(.(FishID, SampleID, get(all_of(varmod))), summarise, readerweight=sum(weight_II)) %>%
     spread(key = "get(all_of(varmod))", value = readerweight)
-  
+
   out[is.na(out)]=0
-  
-  datsel <- out %>% select(-c(FishID))
-  
+
+  datsel <- out %>% select(-c(FishID, SampleID))
+
+
   # Determine modal maturity stage depending on ma_method
   out$modal_negexpweight <-
     if (ma_method == "Mean") {
@@ -133,22 +134,22 @@ add_modal_negexpweight <- function(ad, varmod, ma_method) {
               }
             })
     }
-  
-  
+
+
   countcases=vector(length=dim(out)[1])
   for(e in 1:dim(out)[1])
   {
     sel=out[e,]
     df=sel[,-c(1,2,dim(out)[2])]
     max=max(df)
-    countcases[e]=length(df[which(df==max & df!=0)])
+    countcases[e]=length(df[which(df==max)])
   }
-  
+
   out$NModes_negexp=countcases
-  
+
   colnames(out)=c(names(out)[-c((length(names(out))-1):length(names(out)))], paste0("modal_negexpweight_", varmod), paste0("NModes_negexpweight_", varmod))
   # merge CV and modal age to data
-  right_join(ad, out, by = "FishID")
+  right_join(ad, out, by = c("FishID", "SampleID"))
 }
 
 
@@ -156,21 +157,19 @@ add_modal_negexpweight <- function(ad, varmod, ma_method) {
 
 select_mode=function(ad, ma_method, mode_definition){
 
-  dat1=ad[ad$TypeAnnotation=="eventOrganizer"& ad$DoesSampleHaveHistologyImage=="Yes",] %>%
+  dat1=ad[ad$TypeAnnotation=="eventOrganizer" & toupper(ad$DoesSampleHaveHistologyImage)=="YES",] %>%
     mutate(modal_maturity=Maturity, modal_sex=Sex) %>%
-    #select(FishID, SampleID, modal_maturity, modal_sex) %>%
-    select(FishID, modal_maturity, modal_sex) %>%
+    select(FishID, SampleID, modal_maturity, modal_sex) %>%
     distinct()
-  
-  #selad=ad[ad$SampleID %in% setdiff(ad$SampleID, dat1$SampleID), ]
-  selad=ad[ad$FishID %in% setdiff(ad$FishID, dat1$FishID), ]
+
+  selad=ad[ad$SampleID %in% setdiff(ad$SampleID, dat1$SampleID), ]
 
   if(mode_definition=="multistage")
     {
   dat2=selad[selad$TypeAnnotation=="reader",] %>%
-     # select(FishID, SampleID, modal_trad_Maturity, NModes_trad_Maturity, modal_linearweight_Maturity, NModes_linearweight_Maturity, modal_negexpweight_Maturity, NModes_negexpweight_Maturity) %>%
-    select(FishID, modal_trad_Maturity, NModes_trad_Maturity, modal_linearweight_Maturity, NModes_linearweight_Maturity, modal_negexpweight_Maturity, NModes_negexpweight_Maturity) %>%  
-    distinct()
+      select(FishID, SampleID, modal_trad_Maturity, NModes_trad_Maturity, modal_linearweight_Maturity, NModes_linearweight_Maturity,
+      modal_negexpweight_Maturity, NModes_negexpweight_Maturity) %>%
+      distinct()
   dat2$modal_maturity <-
       if (ma_method == "Mean") {
         stop ("mean not implemented yet")
@@ -185,11 +184,12 @@ select_mode=function(ad, ma_method, mode_definition){
               })
       }
   dat2=dat2 %>%
-    select(FishID, modal_maturity)
-  
+    select(FishID, SampleID, modal_maturity)
+
+
   dat3=selad[selad$TypeAnnotation=="reader",] %>%
-   # select(FishID, SampleID, modal_trad_Sex, NModes_trad_Sex, modal_linearweight_Sex, NModes_linearweight_Sex, modal_negexpweight_Sex, NModes_negexpweight_Sex) %>%
-    select(FishID, modal_trad_Sex, NModes_trad_Sex, modal_linearweight_Sex, NModes_linearweight_Sex, modal_negexpweight_Sex, NModes_negexpweight_Sex) %>%
+    select(FishID, SampleID, modal_trad_Sex, NModes_trad_Sex, modal_linearweight_Sex, NModes_linearweight_Sex, modal_negexpweight_Sex,
+    NModes_negexpweight_Sex) %>%
     distinct()
   dat3$modal_sex <-
     if (ma_method == "Mean") {
@@ -205,17 +205,16 @@ select_mode=function(ad, ma_method, mode_definition){
             })
     }
   dat3=dat3 %>%
-    #select(FishID, SampleID, modal_sex)
-    select(FishID, modal_sex)
+  select(FishID, SampleID, modal_sex)
 
-  #dat4=merge(dat2, dat3, by.x=c("FishID", "SampleID"), by.y=c("FishID", "SampleID"))
-  dat4=merge(dat2, dat3, by.x="FishID", by.y="FishID")
-  
+
+  dat4=merge(dat2, dat3, by.x=c("FishID", "SampleID"), by.y=c("FishID", "SampleID"))
+
+
   } else {
-    
+
     dat2=selad[selad$TypeAnnotation=="reader",] %>%
-      #select(FishID, SampleID, modal_trad_Maturity, NModes_trad_Maturity) %>%
-      select(FishID, modal_trad_Maturity, NModes_trad_Maturity) %>%
+      select(FishID, SampleID, modal_trad_Maturity, NModes_trad_Maturity) %>%
       distinct()
     dat2$modal_maturity <-
       if (ma_method == "Mean") {
@@ -231,14 +230,11 @@ select_mode=function(ad, ma_method, mode_definition){
               })
       }
     dat2=dat2 %>%
-      #select(FishID, SampleID, modal_maturity)
-      select(FishID, modal_maturity)
-    
-    
-    
+      select(FishID, SampleID, modal_maturity)
+
+
     dat3=selad[selad$TypeAnnotation=="reader",] %>%
-      #select(FishID, SampleID, modal_trad_Sex, NModes_trad_Sex) %>%
-      select(FishID, modal_trad_Sex, NModes_trad_Sex) %>%
+      select(FishID, SampleID, modal_trad_Sex, NModes_trad_Sex) %>%
       distinct()
     dat3$modal_sex <-
       if (ma_method == "Mean") {
@@ -254,19 +250,16 @@ select_mode=function(ad, ma_method, mode_definition){
               })
       }
     dat3=dat3 %>%
-      #select(FishID, SampleID, modal_sex)
-       select(FishID, modal_sex)
-    #dat4=merge(dat2, dat3, by.x=c("FishID", "SampleID"), by.y=c("FishID", "SampleID"))
-    dat4=merge(dat2, dat3, by.x="FishID", by.y="FishID")
+      select(FishID, SampleID, modal_sex)
+
+    dat4=merge(dat2, dat3, by.x=c("FishID", "SampleID"), by.y=c("FishID", "SampleID"))
   }
 
   dat=rbind(dat1, dat4)
-  
- # ad=distinct(right_join(ad, dat, by = c("FishID", "SampleID")))
-#  ad=distinct(right_join(ad, dat, by ="FishID"))
-  
-   ad=distinct(merge(ad, dat, by.x="FishID", by.y="FishID"))
+
+  ad=distinct(right_join(ad, dat, by = c("FishID", "SampleID")))
 
   return(ad)
 
 }
+
